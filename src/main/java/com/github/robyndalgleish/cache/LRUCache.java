9593aaa5beb84b132c3f323097de45bcce333ac7
robyndalgleish <robyn.dalgleish@groupbyinc.com>
2020-10-21 22:45:51 -0400

    Replace Node with LinkedList<Map.Entry<K, V>>

--- src/main/java/com/github/robyndalgleish/cache/LRUCache.java
+++ src/main/java/com/github/robyndalgleish/cache/LRUCache.java
@@ -1,97 +1,62 @@
 package com.github.robyndalgleish.cache;
 
+import java.awt.*;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
-public class LRUCache {
+public class LRUCache<K, V> {
 
     // We need to access a particular node at any place in the list, but we only need to keep track of the newest and oldest. So, field them fuckers.
-    Node newest;
-    Node oldest;
+    LinkedList<Map.Entry<K, V>> linkedList = new LinkedList<>();
 
     // This thing is essentially a map, so like, map.
-    HashMap<Integer, Node> map;
+    Map<K, V> map = new HashMap<>();
 
     // The capacity stored from dah constructor comment which is next, just read that...
-    int capacity;
+    private final int capacity;
+    private int length;
 
     // The only thing we really need to define before implementing this fucker, is the capacity. Sew... pass that shit in da constructor.
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.map = new HashMap<>();
+        this.length = 0;
     }
 
     // We essentially want a get method and a put method, so let's do that:
 
     // Put sumfin' in the cache:
-    public void put(int key, int value) {
+    public void put(K key, V value) {
         // If the map has this key, just update the map
         if(map.containsKey(key)){
 
-            Node node = map.get(key);
-            node.value = value;
-
             // LinkedList reordering to put the node at the newest position
-            pullNode(node);
-            pushNode(node);
+            linkedList.remove(Map.entry(key, value));
 
-        //delete oldest node in map if we are above capacity with this new addition
+        //delete oldest in map if we are above capacity with this new addition
         }else{
-            if(map.size()>=capacity){
-                map.remove(oldest.key);
-                pullNode(oldest);
+            if(length >= capacity){
+                var removed = linkedList.removeLast();
+                map.remove(removed.getKey());
+                length --;
             }
-
-            //add new node to map
-            Node node = new Node(key, value);
-            pushNode(node);
-            map.put(key, node);
         }
+        //add new key/value pair to map
+        map.put(key, value);
+        length  ++;
+        linkedList.addFirst(Map.entry(key, value));
     }
 
     // Get that cached value:
-    public int get(int key) {
-        if(map.get(key)==null){
-            return -1;
+    public V get(K key) {
+        V value = map.get(key);
+        if (value != null) {
+            linkedList.remove(Map.entry(key, value));
+            linkedList.addFirst(Map.entry(key, value));
+            return value;
         }
-
-        Node node = map.get(key);
-        // Reorder LinkedList to move gotten Node to newest
-        pushNode(node);
-        pushNode(node);
-
-        return node.value;
-    }
-
-    void pullNode(Node node) {
-        // if a node previous to the one we are updating exists
-        // update previous node
-        if(node.previous != null){
-            // then the previous node now points to the node after the one we are updating
-            node.previous.next = node.next;
-        } else {
-            // else the new newest is the one after the node we are updating
-            oldest = node.next;
-        }
-
-        // if the node next to the node we are updating exists (gotta set it right too)
-        if(node.next != null){
-            node.next.previous = node.next;
-        } else {
-            newest = node.previous;
-        }
-    }
-
-    void pushNode(Node node) {
-        if(newest!=null){
-            newest.next = node;
-        }
-
-        node.previous = newest;
-        node.next = null;
-        newest = node;
-
-        if(oldest == null){
-            oldest = newest;
-        }
+        throw new NoSuchElementException("Naw brah; that ain't a thing.");
     }
 }
--- src/test/java/com/github/robyndalgleish/cache/LRUCacheTest.java
+++ src/test/java/com/github/robyndalgleish/cache/LRUCacheTest.java
@@ -1,12 +1,15 @@
 package com.github.robyndalgleish.cache;
 
-import junit.framework.TestCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.Serializable;
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
@@ -18,21 +21,21 @@ public class LRUCacheTest {
     void doesTheThing(){
         // Given: A cache (Map<K, V>) with a limit of 5 entries, and an array (ArrayList<K>(5)) keeping the order of these entries.
         // LRUCache.put(), if given a key and a value, will put key/value pairs into the map and the keys into the array.
-        LRUCache cache = new LRUCache(3);
+        LRUCache<Serializable, Serializable> cache = new LRUCache<>(3);
         assertThat(cache.map.isEmpty()).isTrue();
         cache.put(1, 1);
-        cache.put(2, 2);
+        cache.put("yea", 2);
         cache.put(3, 3);
         assertThat(cache.map.size()).isEqualTo(3);
 
         // When: We put another entry in.
         cache.put(4, 4);
+        cache.put(3, "shut up");
+        cache.put(5, 5);
 
         // Then: The oldest entry is replaced by the new one.
-        assertThat(cache.newest.value).isEqualTo(4);
-        assertThat(cache.oldest.value).isEqualTo(2);
+        assertThatThrownBy(() -> cache.get(2)).isInstanceOf(NoSuchElementException.class).hasMessage("Naw brah; that ain't a thing.");
+        assertThat(cache.get(4)).isEqualTo(4);
         assertThat(cache.map.size()).isEqualTo(3);
     }
-
-    // Check that get request refreshes queried entry
 }
