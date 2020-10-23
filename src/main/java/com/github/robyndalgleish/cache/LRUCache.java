1f2da31bbf322c8218dcd6a023ba1801112e3f21
robyndalgleish <robyn.dalgleish@groupbyinc.com>
2020-10-23 00:30:20 -0400

    Replace LinkedList<Map.Entry<K, V>> with LinkedHashMap

--- src/main/java/com/github/robyndalgleish/cache/LRUCache.java
+++ src/main/java/com/github/robyndalgleish/cache/LRUCache.java
@@ -1,28 +1,19 @@
 package com.github.robyndalgleish.cache;
 
-import java.awt.*;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.NoSuchElementException;
 
 public class LRUCache<K, V> {
 
-    // We need to access a particular node at any place in the list, but we only need to keep track of the newest and oldest. So, field them fuckers.
-    LinkedList<Map.Entry<K, V>> linkedList = new LinkedList<>();
-
-    // This thing is essentially a map, so like, map.
-    Map<K, V> map = new HashMap<>();
+    LinkedHashMap<K, V> map = new LinkedHashMap<>();
 
     // The capacity stored from dah constructor comment which is next, just read that...
     private final int capacity;
-    private int length;
 
     // The only thing we really need to define before implementing this fucker, is the capacity. Sew... pass that shit in da constructor.
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.length = 0;
     }
 
     // We essentially want a get method and a put method, so let's do that:
@@ -33,28 +24,26 @@ public class LRUCache<K, V> {
         if(map.containsKey(key)){
 
             // LinkedList reordering to put the node at the newest position
-            linkedList.remove(Map.entry(key, value));
+            map.remove(key, value);
 
         //delete oldest in map if we are above capacity with this new addition
         }else{
-            if(length >= capacity){
-                var removed = linkedList.removeLast();
-                map.remove(removed.getKey());
-                length --;
+            if(map.size() >= capacity){
+                Iterator<K> iterator = map.keySet().iterator();
+                iterator.next();
+                iterator.remove();
             }
         }
         //add new key/value pair to map
         map.put(key, value);
-        length  ++;
-        linkedList.addFirst(Map.entry(key, value));
     }
 
     // Get that cached value:
     public V get(K key) {
         V value = map.get(key);
         if (value != null) {
-            linkedList.remove(Map.entry(key, value));
-            linkedList.addFirst(Map.entry(key, value));
+            map.remove(key, value);
+            map.put(key, value);
             return value;
         }
         throw new NoSuchElementException("Naw brah; that ain't a thing.");
